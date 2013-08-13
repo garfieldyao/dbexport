6ca4381a47446c6d0f16fd739a72cace5a62bd50
garfieldyao <garfieldyao@gmail.com>
2013-08-13 23:15:30 +0800

    gui

---
+++ src/main/java/com/mars/dbexporter/gui/AppMainFrame.java
@@ -0,0 +1,8 @@
+package com.mars.dbexporter.gui;
+
+import javax.swing.JFrame;
+
+public class AppMainFrame extends JFrame {
+	private static final long serialVersionUID = -3503038684028459652L;
+
+}
