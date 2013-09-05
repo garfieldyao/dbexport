34d91d53080f66a1da981c3ef1a19f82e4c5178a
garfieldyao <garfieldyao@gmail.com>
2013-09-05 23:16:24 +0800

    mmm

---
+++ src/main/java/com/mars/dbexport/bo/MibConst.java
@@ -0,0 +1,22 @@
+package com.mars.dbexport.bo;
+
+public class MibConst {
+	public final static String asamMibVersion = "1.3.6.1.4.1.637.61.1.9.13.0";
+
+	public final static String asamDbmUploadServerID = "1.3.6.1.4.1.637.61.1.24.2.6.0";
+	public final static String asamDbmUploadPath = "1.3.6.1.4.1.637.61.1.24.2.7.0";
+	public final static String asamDbmUploadDatabaseSelection = "1.3.6.1.4.1.637.61.1.24.2.8.0";
+	public final static String asamDbmUploadProgress = "1.3.6.1.4.1.637.61.1.24.2.9.0";
+
+	public final static String fileTransferProtocolSelect = "1.3.6.1.4.1.637.61.1.74.1.0";
+	public final static String fileTransferServerID = "1.3.6.1.4.1.637.61.1.74.2.1.1";
+	public final static String fileTransferServerUsername = "1.3.6.1.4.1.637.61.1.74.2.1.2";
+	public final static String fileTransferServerPassword = "1.3.6.1.4.1.637.61.1.74.2.1.3";
+	public final static String fileTransferServerRowStatus = "1.3.6.1.4.1.637.61.1.74.2.1.4";
+
+	public final static int FTP_MODE = 2;
+	public final static int ACTIVE_DB = 1;
+	public final static int ongoing = 1;
+	public final static int succeed = 2;
+	public final static int failed = 3;
+}
